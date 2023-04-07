ff370254725700970dfc64669d5345a57924fc75
madam_kuraga <Tanushasergeevna@iCloud.com>
2023-04-07 09:54:48 +0300

    kodnew

--- prodject/game_v2.py
+++ prodject/game_v2.py
@@ -1,51 +1,27 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
 import numpy as np
 
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
-
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
-
-    count = 0
-
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
-        if number == predict_number:
-            break # выход из цикла, если угадали
-    return(count)
-
-print(f'Количество попыток: {random_predict()}')
-
-def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
-
-    Args:
-        random_predict ([type]): функция угадывания
-
-    Returns:
-        int: среднее количество попыток
-    """
-
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
-
-    for number in random_array:
-        count_ls.append(random_predict(number))
-
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
-
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
-
-# RUN
-if __name__ == '__main__':
-    score_game(random_predict)  
+def guess_number(number):
+    min = 1
+    max = 1000
+    guess = None
+    attempts = 0
+    max_attemps = 20
     
+    while attempts < max_attemps:
+        guess = (min + max) // 2
+        attempts += 1
+
+        if guess == number:
+            return f"Загаданное число {number} найдено за {attempts} попыток"
+        elif guess < number:
+            min = guess + 1
+        else:
+            max = guess - 1
+
+    return f"Загаданное число {number} не было найдено за {max_attemps} попыток"
     
-     
+print(guess_number(20))    
